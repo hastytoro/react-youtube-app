63eb311aafb61d49e765d307e857304ed251ccb9
Luigi Lupini <luigi.lupini@gmail.com>
2022-09-07 09:50:33 +0200

    5) Rendering individual items from <VideoList />

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
+
 import SearchBar from "./components/SearchBar";
+import VideoList from "./components/VideoList";
 
 import youtube from "./apis/youtube";
 
@@ -8,24 +10,19 @@ class App extends React.Component {
   // We use this callback prop passed down to our child component that collects
   // inputs state "a searched term" that App can then pass into a API request.
   onTermSubmit = async (term) => {
-    // console.log(term); // from child input component <SearchBar />.
+    // The `term` from child input component <SearchBar />.
     // We use our pre-configured axios instance to the following path/endpoint:
     // The term we using in our youtube API query is coming from child state.
-    // Check network tab in DevTools to determine if fetch request worked!
     const response = await youtube.get("/search", {
       params: { q: term },
     });
     this.setState({ videos: response.data.items });
   };
-
-  componentDidUpdate() {
-    console.log(this.state);
-  }
-
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
---
+++ src/components/VideoList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import VideoItem from "./VideoItem";
+
+const VideoList = ({ videos }) => {
+  const renderedList = videos.map((video) => {
+    return <VideoItem key={video.id.videoId} video={video} />;
+  });
+  return <div className="ui list relaxed divided">{renderedList}</div>;
+};
+
+export default VideoList;
