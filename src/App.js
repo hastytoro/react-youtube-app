24192f54544ff866342c5d44a63b627bcee15fdb
Luigi Lupini <luigi.lupini@gmail.com>
2022-09-16 12:55:18 +0200

    - Refactoring the App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import SearchBar from "./components/SearchBar";
 import VideoList from "./components/VideoList";
@@ -6,53 +6,42 @@ import VideoDetail from "./components/VideoDetail";
 
 import youtube from "./apis/youtube";
 
-class App extends React.Component {
-  state = { videos: [], selectedVideo: null };
-
-  componentDidMount() {
-    this.onTermSubmit("racing");
-  }
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
   // We use this callback prop passed down to our child component that collects
   // inputs state "a searched term" that App can then pass into a API request.
-  onTermSubmit = async (term) => {
-    // Retrieve `term` from child input component <SearchBar />.
-    // The term we using in our youtube API query is coming from child state.
+  // Retrieve `term` from child input component <SearchBar />.
+  // The term we using in our youtube API query is coming from child state.
+  const onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
       params: { q: term },
     });
     const { items } = response.data;
-    this.setState({
-      videos: items,
-      selectedVideo: items[0],
-    });
-  };
-  // Again here we adding another callback method to the App component.
-  // We pass a prop as reference down first to VideoList then to VideoItem.
-  // This will be called back with information of the selected item.
-  onVideoSelect = (video) => {
-    this.setState({ selectedVideo: video });
+    setVideos(items);
+    setSelectedVideo(items[0]);
   };
 
-  render() {
-    return (
-      <div className="ui container">
-        <SearchBar onTermSubmit={this.onTermSubmit} />
-        <div className="ui grid">
-          <div className="ui row">
-            <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo} />
-            </div>
-            <div className="five wide column">
-              <VideoList
-                onVideoSelect={this.onVideoSelect}
-                videos={this.state.videos}
-              />
-            </div>
+  useEffect(() => {
+    onTermSubmit("skysports");
+  }, []);
+
+  return (
+    <div className="ui container">
+      <SearchBar onTermSubmit={onTermSubmit} />
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList onVideoSelect={setSelectedVideo} videos={videos} />
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 export default App;
